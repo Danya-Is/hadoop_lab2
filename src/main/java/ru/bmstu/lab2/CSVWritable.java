f835a6b8cf70db26c01242806e754f4277519fa1
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 14:39:36 +0300

    Scripted auto-commit on change (2021-10-14 14:39:36) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/CSVWritable.java
+++ src/main/java/ru/bmstu/lab2/CSVWritable.java
@@ -1,4 +1,20 @@
 package ru.bmstu.lab2;
 
-public class CSVWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class CSVWritable extends Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
