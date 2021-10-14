f6e2c858ac6ed21d4cc26c8e41e1441eeae17bcd
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 21:39:21 +0300

    Scripted auto-commit on change (2021-10-14 21:39:21) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportWritableComparable.java
+++ src/main/java/ru/bmstu/lab2/AirportWritableComparable.java
@@ -18,11 +18,13 @@ public class AirportWritableComparable implements WritableComparable {
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.write();
+        dataOutput.write(airportID);
+        dataOutput.write(indicator);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        dataInput.readInt();
+        dataInput.readInt();
     }
 }
