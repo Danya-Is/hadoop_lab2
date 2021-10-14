6476bb158d18e8d3fdcb967b8561213a3d69cc9a
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 21:57:15 +0300

    Scripted auto-commit on change (2021-10-14 21:57:15) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 
-    public static int AIRPORT_CODE_POS = 0;
+    public static int AIRPORT_ID_POS = 0;
     public static int AIRPORT_NAME_POS = 1;
     public static int INDICATOR = 0;
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         String[] airportFeatures = value.toString().split(",");
-        int airportID = Integer.parseInt(airportFeatures[AIRPORT_CODE_POS]);
+        int airportID = Integer.parseInt(airportFeatures[AIRPORT_ID_POS]);
         String airportName = airportFeatures[AIRPORT_NAME_POS];
         context.write(new AirportWritableComparable(airportID, INDICATOR), new Text(airportName));
     }
--- src/main/java/ru/bmstu/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightMapper.java
@@ -7,15 +7,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    public static int AIRPORT_CODE_POS = 0;
-    public static int AIRPORT_NAME_POS = 1;
+    public static int AIRPORT_ID_POS = 14;
+    public static int AIRPORT_DELAY_POS = 18;
     public static int INDICATOR = 1;
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         String[] airportFeatures = value.toString().split(",");
-        int airportID = Integer.parseInt(airportFeatures[AIRPORT_CODE_POS]);
-        String airportName = airportFeatures[AIRPORT_NAME_POS];
+        int airportID = Integer.parseInt(airportFeatures[AIRPORT_ID_POS]);
+        String airportDelay = airportFeatures[AIRPORT_DELAY_POS];
         context.write(new AirportWritableComparable(airportID, INDICATOR), new Text(airportName));
     }
 }
