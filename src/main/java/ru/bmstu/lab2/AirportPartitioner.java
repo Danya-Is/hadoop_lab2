044e61b3d407beeb32edbd33bde28e505983378d
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 22:13:32 +0300

    Scripted auto-commit on change (2021-10-14 22:13:32) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportPartitioner.java
+++ src/main/java/ru/bmstu/lab2/AirportPartitioner.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
     @Override
     public int getPartition(AirportWritableComparable key, Text value, int i) {
-        return key.getAirportID();
+        return key.getAirportID() % ;
     }
 }
