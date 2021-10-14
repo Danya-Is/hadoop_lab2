e58e6b1ee7fde09740b9672e5e0f46cb32b0c96d
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 21:46:57 +0300

    Scripted auto-commit on change (2021-10-14 21:46:57) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 
-    private static 
+    public static int AIRPORT_CODE_POS = 0;
+    public static int AIRPORT_NAME_POS = 1;
+    public static int INDICATOR = 
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
