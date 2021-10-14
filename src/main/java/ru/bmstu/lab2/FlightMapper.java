4fa8f8abb8c08fa0bf6be2897e140004991621dd
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 21:51:21 +0300

    Scripted auto-commit on change (2021-10-14 21:51:21) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightMapper.java
@@ -1,4 +1,21 @@
 package ru.bmstu.lab2;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    public static int AIRPORT_CODE_POS = 0;
+    public static int AIRPORT_NAME_POS = 1;
+    public static int INDICATOR = 0;
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
+        String[] airportFeatures = value.toString().split(",");
+        int airportID = Integer.parseInt(airportFeatures[AIRPORT_CODE_POS]);
+        String airportName = airportFeatures[AIRPORT_NAME_POS];
+        context.write(new AirportWritableComparable(airportID, INDICATOR), new Text(airportName));
+    }
 }
