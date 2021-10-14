105c962ada1024ebe6c1b7eed9d8ec2a9999bc3d
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 21:45:01 +0300

    Scripted auto-commit on change (2021-10-14 21:45:01) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -7,6 +7,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+
+    private static 
+
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         String[] airportFeatures = value.toString().split(",");
