f925cf354da8e6c3ae7992bfec9fbd64f74c372e
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 15:21:25 +0300

    Scripted auto-commit on change (2021-10-14 15:21:25) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/JoinApp.java
+++ src/main/java/ru/bmstu/lab2/JoinApp.java
@@ -3,7 +3,6 @@ package ru.bmstu.lab2;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -13,8 +12,8 @@ public class JoinApp {
         Job job = Job.getInstance();
         job.setJarByClass(JoinApp.class);
         job.setJobName("Join App");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
     }
 }
