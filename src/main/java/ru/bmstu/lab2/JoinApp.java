307f29905578aa87907658e9e6e608b799a27d49
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 14:21:05 +0300

    Scripted auto-commit on change (2021-10-14 14:21:05) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/JoinApp.java
+++ src/main/java/ru/bmstu/lab2/JoinApp.java
@@ -1,7 +1,10 @@
 package main.java.ru.bmstu.lab2;
 
+
+import org.apache.hadoop.mapreduce.Job;
+
 public class JoinApp {
     public static void main(String[] args) throws Exception {
-        
+        Job job = Job.getInstance();
     }
 }
