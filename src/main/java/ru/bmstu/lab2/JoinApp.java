eda2a3d6d81eb8f72d1e4c313694950819a9deed
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-14 14:24:08 +0300

    Scripted auto-commit on change (2021-10-14 14:24:08) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/JoinApp.java
+++ src/main/java/ru/bmstu/lab2/JoinApp.java
@@ -2,9 +2,13 @@ package ru.bmstu.lab2;
 
 
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 public class JoinApp {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
+        job.setJarByClass(JoinApp.class);
+        job.setJobName("Join App");
+        FileInputFormat.addInputPath(job, new Path());
     }
 }
