327298a8eb75d1e417b8d812bafe473fd37a5f60
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-15 11:21:19 +0300

    Scripted auto-commit on change (2021-10-15 11:21:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/lab2/AirportReducer.java
@@ -4,11 +4,17 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
 
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+        Iterator<Text> airportIter = values.iterator();
+        Text airportName = new Text(airportIter.next());
+        int min = Integer.MAX_VALUE, max = 0;
+        while (airportIter.hasNext()) {
+            float delay = Float.parseFloat(airportIter.next().toString());
+        }
     }
 }
