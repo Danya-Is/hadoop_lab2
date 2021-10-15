37d151605059ef78aa7677e8fee29eb65f3f4a76
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-15 14:28:08 +0300

    Scripted auto-commit on change (2021-10-15 14:28:08) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/GroupingComparator.java
+++ src/main/java/ru/bmstu/lab2/GroupingComparator.java
@@ -6,9 +6,9 @@ import org.apache.hadoop.io.WritableComparator;
 public class GroupingComparator extends WritableComparator {
 
     public GroupingComparator() {
-
+        super(AirportWritableComparable.class, true);
     }
-    
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return Integer.compare(((AirportWritableComparable) a).getAirportID(), ((AirportWritableComparable) b).getAirportID());
