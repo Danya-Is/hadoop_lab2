f9b5a8d4a35a0e3bc8b19ada2ea5c088958cde36
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-15 14:22:54 +0300

    Scripted auto-commit on change (2021-10-15 14:22:54) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/lab2/AirportReducer.java
@@ -11,22 +11,25 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Reducer<AirportWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
         Iterator<Text> airportIter = values.iterator();
-        Text airportName = new Text(airportIter.next());
-        float minDelay = Integer.MAX_VALUE, maxDelay = 0, sum = 0;
-        int count = 0;
-        while (airportIter.hasNext()) {
-            float delay = Float.parseFloat(airportIter.next().toString());
-            sum += delay;
-            count++;
-            if (delay < minDelay)
-                minDelay = delay;
-            if (delay > maxDelay)
-                maxDelay = delay;
+        if (airportIter.hasNext()) {
+            Text airportName = new Text(airportIter.next());
+            float minDelay = Integer.MAX_VALUE, maxDelay = 0, sum = 0;
+            int count = 0;
+            while (airportIter.hasNext()) {
+                float delay = Float.parseFloat(airportIter.next().toString());
+                sum += delay;
+                count++;
+                if (delay < minDelay)
+                    minDelay = delay;
+                if (delay > maxDelay)
+                    maxDelay = delay;
+            }
+            float averageDelay = sum / count;
+            if (count > 0)
+                context.write(airportName, new Text("Average delay is " + averageDelay + " minutes\n" +
+                        "Minimum delay is " + minDelay + " minutes\n" +
+                        "Maximum delay is " + maxDelay + " minutes\n"));
         }
-        float averageDelay = sum / count;
-        if (count > 0)
-            context.write(airportName, new Text("Average delay is " + averageDelay + " minutes\n" +
-                    "Minimum delay is " + minDelay + " minutes\n" +
-                    "Maximum delay is " + maxDelay + " minutes\n"));
+
     }
 }
