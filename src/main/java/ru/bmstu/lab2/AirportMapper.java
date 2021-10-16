8d109a0f698efbb4f523287d65dca495e6e9eeba
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-16 08:53:48 +0300

    Scripted auto-commit on change (2021-10-16 08:53:48) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -8,13 +8,14 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 
+    public static final String DELIMETR = "\",\"";
     public static int AIRPORT_ID_POS = 0;
     public static int AIRPORT_NAME_POS = 1;
     public static int INDICATOR = 0;
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
-        String[] airportFeatures = value.toString().split("\",\"");
+        String[] airportFeatures = value.toString().split(DELIMETR);
         if (key.get() > 0) {
             int airportID = Integer.parseInt(airportFeatures[AIRPORT_ID_POS].replaceAll("\"", ""));
             String airportName = airportFeatures[AIRPORT_NAME_POS].replaceAll("\"", "");
