6d59db76e1ca651a6ac65e5e09d5c6be07796807
Ismagilova Daria <armi.din1902@gmail.com>
2021-10-16 08:52:16 +0300

    Scripted auto-commit on change (2021-10-16 08:52:16) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightMapper.java
@@ -7,13 +7,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    public static final String DELIMETER = ",";
     public static int AIRPORT_ID_POS = 14;
     public static int AIRPORT_DELAY_POS = 18;
     public static int INDICATOR = 1;
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context) throws IOException, InterruptedException {
-        String[] airportFeatures = value.toString().split(",");
+        String[] airportFeatures = value.toString().split(DELIMETER);
         if (key.get() > 0) {
             int airportID = Integer.parseInt(airportFeatures[AIRPORT_ID_POS]);
             String airportDelay = airportFeatures[AIRPORT_DELAY_POS];
